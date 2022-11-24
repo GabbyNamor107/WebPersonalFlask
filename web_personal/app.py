a3e404b8bc0fb7e42087440d4d38899732220b7c
GabbyNamor <taehyung107gabriela@gmail.com>
2022-11-23 19:46:07 -0600

    Implementacion Loginform parte 1

--- web_personal/app.py
+++ web_personal/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, EmailField
 
 app = Flask(__name__)
 app.config['SECRET_KEY']= 'secret'
@@ -50,16 +50,16 @@ def portfolio():
 
 
 ############# Formularios de WTForms #################
-
 class LoginForm(FlaskForm):
-    username = StringField('Username')
+    username = EmailField('Username')
     password = PasswordField('Password')
-    submit = SubmitField('Login'
-    )
+    submit = SubmitField('Ingresar')
+
 ############# Rutas Login ############
 @app.route('/auth/login')
 def login():
-    return render_template('/auth/login.html')
+    form = LoginForm()
+    return render_template('/auth/login.html', form=form)
 
 @app.route('/auth/register')
 def register():
