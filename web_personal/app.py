49f7cc6f236d3e51a8dcc446b31ac90a8b785330
GabbyNamor <taehyung107gabriela@gmail.com>
2022-12-02 19:22:37 -0600

    Blueprint Error Pages con errorhandler

--- web_personal/app.py
+++ web_personal/app.py
@@ -1,22 +1,19 @@
 ######### Imports Flask & Python ###########
-from flask import Flask, render_template, request, url_for
+from flask import Flask
 
-######### Imports Apps Blueprints ##########
+############## Imports Apps Blueprints ##########
 from home.views import home_blueprint
 from auth.views import auth_blueprint
+from error_pages.handlers import error_pages
 
 app = Flask(__name__)
 app.config['SECRET_KEY']= 'secret'
 
 
-@app.errorhandler(404)
-def page_error_not_found(e):
-    return render_template('error/404.html'), 404
-
-
-########### Registro de Apps ###############
+################# Registro de Apps ##################
 app.register_blueprint(home_blueprint)
 app.register_blueprint(auth_blueprint, url_prefix='/auth')
+app.register_blueprint(error_pages)
 
 if __name__ == '__main__':
     app.run(debug=True)
