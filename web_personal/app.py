351b9131ba85c4bfaaec39fe46e1842594fb2e5d
GabbyNamor <taehyung107gabriela@gmail.com>
2022-12-02 18:04:00 -0600

    Blueprint home

--- web_personal/app.py
+++ web_personal/app.py
@@ -1,60 +1,30 @@
 from flask import Flask, render_template, request, url_for
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, EmailField
+from wtforms import (StringField, PasswordField, 
+                    SubmitField, EmailField, 
+                    IntegerField, RadioField,
+                    SelectField, TextAreaField) 
 from wtforms.validators import DataRequired, Email
+from home.views import home_blueprint
 
 app = Flask(__name__)
 app.config['SECRET_KEY']= 'secret'
-
-######### Rutas Public ###########
-@app.route('/')
-def index():
-    return render_template('public/index.html')
-
-@app.route('/about')
-def about():
-    return render_template('public/about.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('public/contact.html')
-
-@app.route('/portfolio')
-def portfolio():
-    projects = [
-        {
-            'name':'Primer proyecto',
-            'description':'As we got further away, it [the Earth] diminished in size. Finally it shrank to the size of a marbie, the most beautiful you can imagine. The beutiful, warm...',
-            'image':'img/home-bg.jpg',
-            'url': 'https://www.google.com'
-        },
-        {
-            'name':'Segundo proyecto',
-            'description':'As we got further away, it [the Earth] diminished in size. Finally it shrank to the size of a marbie, the most beautiful you can imagine. The beutiful, warm...',
-            'image':'img/about-bg.jpg',
-            'url': 'https://www.xataka.com'
-        },
-        {
-            'name':'Primer proyecto',
-            'description':'As we got further away, it [the Earth] diminished in size. Finally it shrank to the size of a marbie, the most beautiful you can imagine. The beutiful, warm...',
-            'image':'img/home-bg.jpg',
-            'url': 'https://www.google.com'
-        },
-        {
-            'name':'Segundo proyecto',
-            'description':'As we got further away, it [the Earth] diminished in size. Finally it shrank to the size of a marbie, the most beautiful you can imagine. The beutiful, warm...',
-            'image':'img/about-bg.jpg',
-            'url': 'https://www.xataka.com'
-        },
-    ]
-    return render_template('public/portfolio.html', projects=projects)
-
 
 ############# Formularios de WTForms #################
 class LoginForm(FlaskForm):
     email = EmailField('Correo', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Ingresar')
+
+class registerForm(FlaskForm):
+    name = StringField('Nombre')
+    las_name = StringField('Apellidos')
+    email = EmailField('Correo')
+    password = PasswordField('Contraseña')
+    phone = IntegerField('Telefono')
+    is_married = RadioField('Estado Civil', choices=[('True', 'Casado'), ('False', 'Soltero')])
+    gender = SelectField('Genero', choices=[('male', 'Masculino'), ('female', 'Femenino'), ('other', 'Otro')])
+    submit = SubmitField('Registrar')
 
 ############# Rutas Login ############
 @app.route('/auth/login', methods=['GET', 'POST'])
@@ -71,24 +41,19 @@ def login():
 
 @app.route('/auth/register')
 def register():
-    return render_template('auth/register.html')  
 
-@app.route('/welcome', methods=['GET', 'POST'])
-def welcome(form):
+    form = RegisterForm()
 
-    if form.validate_on_submit():
-        email = form.email.data
-        password = form.password.data
-
-        return render_template('admin/index.html', email=email)
-
-    return redirect(url_for('login'))
-
+    return render_template('auth/register.html', form=form)  
 
 
 @app.errorhandler(404)
 def page_error_not_found(e):
     return render_template('error/404.html'), 404
+
+
+########### Apps ###############
+@app.register_blueprint(home_blueprint)
 
 if __name__ == '__main__':
     app.run(debug=True)
